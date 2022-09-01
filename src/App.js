da40a4a3f982a815ebc57ecfdca77222c6aa192f
Huy Phan <phan.q.huy21@gmail.com>
2022-09-01 16:39:37 -0400

    changed structure to page formatting

--- src/App.js
+++ src/App.js
@@ -1,105 +1,28 @@
-import * as React from "react";
-import { useState, useEffect } from "react";
-import { db } from "./firebase-config";
-import { collection, getDocs, addDoc, updateDoc, deleteDoc, doc } from "firebase/firestore";
-import { Button, TextField, Stack, Box } from "@mui/material";
+import React from 'react'
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+import AddRest from './pages/AddRest'
 
-function App() {
-  const [restaurants, setRestaurants] = useState([]);
-  const restaurantDB = collection(db, "restaurants");
-
-  const [newName, setNewName] = useState("");
-  const [newType, setNewType] = useState("");
-  const [newLocation, setNewLocation] = useState("");
-  const [newHours, setNewHours] = useState("");
-
-  useEffect(() => {
-    const getRestaurants = async () => {
-      const data = await getDocs(restaurantDB);
-      setRestaurants(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
-    };
-
-    getRestaurants();
-  }, []);
-
-  const createRestaurant = async () => {
-    try {
-      await addDoc(restaurantDB, {
-        Name: newName,
-        Type: newType,
-        Location: newLocation,
-        Hours: newHours,
-      });
-      console.log("Restaurant created sucessfully"); 
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
+const App = () => {
   return (
-    <div className="App">
-      <Box
-        display="flex"
-        flexDirection="column"
-        justifyContent="center"
-        mt={5}
-        mx={"auto"}
-      >
-        <Stack
-          spacing={2}
-          mx={4}
-          alignItems="center"
-          justifyContent="center"
-          direction="column"
-        >
-          <TextField
-            variant="outlined"
-            label="Name"
-            onChange={(e) => {
-              setNewName(e.target.value);
-            }}
-          />
-          <TextField
-            variant="outlined"
-            label="Type"
-            onChange={(e) => {
-              setNewType(e.target.value);
-            }}
-          />
-          <TextField
-            variant="outlined"
-            label="Location"
-            onChange={(e) => {
-              setNewLocation(e.target.value);
-            }}
-          />
-          <TextField
-            variant="outlined"
-            label="Hours"
-            onChange={(e) => {
-              setNewHours(e.target.value);
-            }}
-          />
+    <Router>
+      <div className="App">
+        {/* <Header token={removeToken} /> */}
+        <div className="Content">
+          <Routes>
+            {/* <Route path="/" element={<Navigate to="/login" />} /> */}
 
-          <Button onClick={createRestaurant} variant="contained">
-            Add restaurant
-          </Button>
-        </Stack>
-      </Box>
+            <Route path="/addNewRestaurant" element={ <AddRest />} />
 
-      {restaurants.map((restaurants) => {
-        return (
-          <div>
-            <h1> Name: {restaurants.Name} </h1>
-            <h1> Type: {restaurants.Type} </h1>
-            <h1> Location: {restaurants.Location} </h1>
-            <h1> Hours: {restaurants.Hours} </h1>
-            -----------------------------------------------------
-          </div>
-        );
-      })}
-    </div>
-  );
+          </Routes>
+        </div>
+      </div>
+    </Router>
+  )
 }
 
-export default App;
+export default App
---
+++ src/pages/AddRest.js
@@ -0,0 +1,112 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import { db } from "../firebase-config";
+import {
+  collection,
+  getDocs,
+  addDoc,
+  updateDoc,
+  deleteDoc,
+  doc,
+} from "firebase/firestore";
+import { Button, TextField, Stack, Box } from "@mui/material";
+
+const AddRest = () => {
+  const [restaurants, setRestaurants] = useState([]);
+  const restaurantDB = collection(db, "restaurants");
+
+  const [newName, setNewName] = useState("");
+  const [newType, setNewType] = useState("");
+  const [newLocation, setNewLocation] = useState("");
+  const [newHours, setNewHours] = useState("");
+
+  useEffect(() => {
+    const getRestaurants = async () => {
+      const data = await getDocs(restaurantDB);
+      setRestaurants(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
+
+    getRestaurants();
+  }, []);
+
+  const createRestaurant = async () => {
+    try {
+      await addDoc(restaurantDB, {
+        Name: newName,
+        Type: newType,
+        Location: newLocation,
+        Hours: newHours,
+      });
+      console.log("Restaurant created sucessfully");
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <div className="App">
+      <Box
+        display="flex"
+        flexDirection="column"
+        justifyContent="center"
+        mt={5}
+        mx={"auto"}
+      >
+        <Stack
+          spacing={2}
+          mx={4}
+          alignItems="center"
+          justifyContent="center"
+          direction="column"
+        >
+          <TextField
+            variant="outlined"
+            label="Name"
+            onChange={(e) => {
+              setNewName(e.target.value);
+            }}
+          />
+          <TextField
+            variant="outlined"
+            label="Type"
+            onChange={(e) => {
+              setNewType(e.target.value);
+            }}
+          />
+          <TextField
+            variant="outlined"
+            label="Location"
+            onChange={(e) => {
+              setNewLocation(e.target.value);
+            }}
+          />
+          <TextField
+            variant="outlined"
+            label="Hours"
+            onChange={(e) => {
+              setNewHours(e.target.value);
+            }}
+          />
+
+          <Button onClick={createRestaurant} variant="contained">
+            Add restaurant
+          </Button>
+        </Stack>
+      </Box>
+
+      {restaurants.map((restaurants) => {
+        return (
+          <div>
+            <h1> Name: {restaurants.Name} </h1>
+            <h1> Type: {restaurants.Type} </h1>
+            <h1> Location: {restaurants.Location} </h1>
+            <h1> Hours: {restaurants.Hours} </h1>
+            -----------------------------------------------------
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default AddRest;
